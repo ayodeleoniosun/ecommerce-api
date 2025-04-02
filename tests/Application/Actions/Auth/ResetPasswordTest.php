37c3d8f7fce3ef72354abc2fce2ad1257ba487a2
Ayodele Oniosun <ayodeleoniosun63@gmail.com>
2025-04-02 03:06:28 +0100

    feat: unit test for reset password

--- tests/Application/Actions/Auth/ResetPasswordTest.php
+++ tests/Application/Actions/Auth/ResetPasswordTest.php
@@ -3,15 +3,28 @@
 namespace Tests\Application\Actions\Auth;
 
 use App\Application\Actions\Auth\ResetPassword;
+use App\Application\Shared\Enum\UserEnum;
 use App\Application\Shared\Exceptions\BadRequestException;
 use App\Application\Shared\Exceptions\ResourceNotFoundException;
 use App\Domain\Auth\Interfaces\Repositories\Auth\UserRepositoryInterface;
+use App\Http\Requests\ResetPasswordRequest;
 use App\Infrastructure\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Password;
 use Mockery;
 
 beforeEach(function () {
     $this->userRepo = Mockery::mock(UserRepositoryInterface::class);
     $this->user = User::factory()->create();
+    $this->resetPassword = new ResetPassword($this->userRepo);
+    $this->request = new ResetPasswordRequest;
+    $this->request->merge([
+        'email' => $this->user->email,
+        'token' => 'this_token',
+        'password' => 'new_password',
+        'password_confirmation' => 'new_password',
+    ]);
 });
 
 it('should throw an exception if email is not found', function () {
@@ -20,8 +33,7 @@ it('should throw an exception if email is not found', function () {
         ->with($this->user->email)
         ->andReturn(null);
 
-    $resetPassword = new ResetPassword($this->userRepo);
-    $resetPassword->execute($this->user->email);
+    $this->resetPassword->execute($this->request->all());
 })->throws(ResourceNotFoundException::class, 'Email not found');
 
 it('should throw an exception if user is not yet verified', function () {
@@ -30,8 +42,7 @@ it('should throw an exception if user is not yet verified', function () {
         ->with($this->user->email)
         ->andReturn($this->user);
 
-    $resetPassword = new ResetPassword($this->userRepo);
-    $resetPassword->execute($this->user->email);
+    $this->resetPassword->execute($this->request->all());
 })->throws(BadRequestException::class, 'User not yet verified');
 
 it('should throw an exception if user is not active', function () {
@@ -42,25 +53,31 @@ it('should throw an exception if user is not active', function () {
         ->with($this->user->email)
         ->andReturn($this->user);
 
-    $resetPassword = new ResetPassword($this->userRepo);
-    $resetPassword->execute($this->user->email);
+    $this->resetPassword->execute($this->request->all());
 })->throws(BadRequestException::class, 'User not active');
 
-// it('can reset password', function () {
-//    Event::fake();
-//
-//    $this->user->email_verified_at = now();
-//    $this->user->status = UserEnum::ACTIVE->value;
-//
-//    $this->userRepo->shouldReceive('findByEmail')
-//        ->once()
-//        ->with($this->user->email)
-//        ->andReturn($this->user);
-//
-//    $resetPassword = new ResetPassword($this->userRepo);
-//    $status = $resetPassword->execute($this->user->email);
-//
-//    dd($status);
-//
-//    expect($status)->toBe(Password::RESET_LINK_SENT);
-// });
+it('can reset password', function () {
+    Event::fake();
+
+    $this->user->email_verified_at = now();
+    $this->user->status = UserEnum::ACTIVE->value;
+
+    $this->userRepo->shouldReceive('findByEmail')
+        ->once()
+        ->with($this->user->email)
+        ->andReturn($this->user);
+
+    $this->userRepo->shouldReceive('resetPassword')
+        ->once()
+        ->with($this->request->all())
+        ->andReturn(Password::PASSWORD_RESET);
+
+    $status = $this->resetPassword->execute($this->request->all());
+
+    expect($status)->toBe(Password::PASSWORD_RESET);
+
+    Event::assertDispatched(PasswordReset::class, function ($event) {
+        return $event->user->id === $this->user->id;
+    });
+
+});
